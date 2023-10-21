75a484b04d360682057a67a06b37741e6c132648
Phillip Kataswa <tomphillip756@gmail.com>
2023-10-21 13:47:12 -0400

    add your own tokenKey

--- app/src/comp/front.js
+++ app/src/comp/front.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './front.css';
 
-
+const tokenKey = "";
 
 function Front() {
     const [longURL, setLongURL] = useState('');
